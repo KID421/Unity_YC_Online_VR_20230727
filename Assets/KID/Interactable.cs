514a989e9d904a9c80fe8f8f94dfb6297a4ec8bf
KID <50666207+KID421@users.noreply.github.com>
2023-07-27 16:30:46 +0800

    Update 上傳專案

---
+++ Assets/KID/Interactable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    public class Interactable : MonoBehaviour
+    {
+        [SerializeField, Header("偵測區域")]
+        private string nameTrigger = "偵測區域";
+        [SerializeField, Header("動畫參數名稱")]
+        private string nameAnimation;
+
+        private Animator ani;
+
+        private void Awake()
+        {
+            ani = GetComponent<Animator>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            print($"<color=#9966ff>碰到物件：{other.name}</color>");
+
+            if (other.name.Contains(nameTrigger))
+            {
+                ani.SetTrigger(nameAnimation);
+            }
+        }
+    }
+}
+
